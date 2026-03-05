9b7d40ae714be76e2c8b21d01bcb9e0aa239efe2
manthan <zeus@Zeus.localdomain>
2026-03-05 06:37:23 +0000

    added a fix in terrafform files

--- terraform/namespace.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_namespace" "mern" {
-  metadata {
-    name = "mern-app"
-  }
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,3 @@
 variable "cluster_name" {
   default = "mern-app"
 }
-variable "image_tag" {
-  type    = string
-  default = "latest"
-}
